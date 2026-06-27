0e094e313a8721b2d9d78302fcf807b44e6a4f34
Pablo Martins <pablomartinsti@outlook.com>
2026-06-27 01:29:24 -0300

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { Stack } from "expo-router";
-import { AuthProvider } from "@/contexts/authContext";
+import React from 'react';
+import { Stack } from 'expo-router';
+import { AuthProvider } from '@/contexts/authContext';
+import { useFonts } from 'expo-font';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    ...MaterialIcons.font
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <AuthProvider>
       <Stack
         screenOptions={{
-          headerShown: false, // Oculta o cabeçalho padrão
+          headerShown: false
         }}
       />
     </AuthProvider>
